96e404780c770cefc1e9c641542dee25f5e84df6
kevin <kevinlay.fr@gmail.com>
2015-02-13 20:57:19 +0100

    premiere version

--- index.php
+++ index.php
@@ -1,118 +1,94 @@
-<?php
-    session_start();
- ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
 <head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<title>Portfolio de Kevin Lay Developpeur Front-End.</title>
-	<meta name="description" content="Portfolio de Kevin Lay Developpeur Front-End." />
-	<meta name="author" content="Kevin Lay" />
-	<meta name="viewport" content="width=device-height; max-scale=1.0" />
-	<link rel="icon" type="image/png" href="img/favicon.png" />
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-	<link rel="stylesheet" media="screen" href="css/meyer.css" />
-	<link rel="stylesheet" media="screen" href="css/style.css" />
-</head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Portfolio de Kevin Lay Developpeur Front-End.</title>
+    <meta name="description" content="Portfolio de Kevin Lay Developpeur Front-End." />
+    <meta name="author" content="Kevin Lay" />
+    <meta name="viewport" content="width=device-height; max-scale=1.0" />
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="stylesheet" media="screen" href="css/meyer.css" />
+    <link rel="stylesheet" media="screen" href="css/style.css" />
 </head>
 <body>
-	<section id="content">
-		<header>
-			<nav>
-				<a href="#presentation">Présentation</a>
-				<a href="#competences">Compétences</a>
-				<!-- <a href="#projets">Projets</a> -->
-				<a href="#contact">Contact</a>
-			</nav>
-		</header>
-		<footer>
-			<p>© 2015 Kevin Lay</p>
-			<a href="cv_kevin_lay.pdf" class="cv" title="Imprimer mon cv" download="">CV</a> <!-- Mettre logo  -->
-			<a href="https://twitter.com/Lay_Kev" class="twitter" title="Allez sur mon profil twitter">Twitter</a> <!-- Mettre logo  -->
-			<a href="http://www.linkedin.com/pub/kevin-lay/40/900/836" class="link" title="Visitez mon compte linkedin">Linkedin</a> <!-- Mettre logo  -->
-		</footer>
-		<article id="presentation">
-			<div>
-				<img src="img/logo.png" alt="Logo Kevin Lay" />
-				<h1>Webdesigner / Developpeur Front-End.</h1>
-				<h2>Kevin Lay</h2>
-				<p>Une première année en cursus Chef de Projet de Multimédia à l'IESA Multimédia, valorisé par un stage de 6 mois chez Easyvoyage.com au poste d'intégrateur. Et maintenant en formation continue en tant que développeur Multimédia à l'école Multimédia.</p>
-			</div>
-		</article>
-		<article id="competences">
-			<h1>Compétences</h1>
-			<div class="compColumn">
-				<div class="column html">
-					<h2>HTML</h2>
-				</div>
-				<div class="reflet"></div>
-			</div>
-			<div class="compColumn">
-				<div class="column css">
-					<h2>CSS</h2>
-				</div>
-				<div class="reflet"></div>
-			</div>
-			<div class="compColumn">
-				<div class="column js">
-					<h2>Javascript</h2>
-				</div>
-				<div class="reflet"></div>
-			</div>
-			<div class="compColumn">
-				<div class="column wdp">
-					<h2>Wordpress</h2>
-				</div>
-				<div class="reflet"></div>
-			</div>
-			<div class="compColumn">
-				<div class="column ps">
-					<h2>Photoshop</h2>
-				</div>
-				<div class="reflet"></div>
-			</div>
-		</article>
-			<!-- <figure>
-					<a href=""><img src="img/grand/foliowe.jpg" alt="" class="" data-source="" /></a>
-					<figcaption>Légende associée</figcaption><br />
-				</figure>
-				<figure>
-					<a href=""><img src="img/grand/folio.jpg" alt="" class="" data-source="" /></a>
-					<figcaption>Légende associée</figcaption><br />
-				</figure>
-				<figure>
-					<a href=""><img src="img/grand/hotel.jpg" alt="" class="" data-source="" /></a>
-					<figcaption>Légende associée</figcaption><br />
-				</figure>
-				<figure>
-					<a href=""><img src="img/grand/golf.jpg" alt="" class="" data-source="" /></a>
-					<figcaption>Légende associée</figcaption><br />
-				</figure> -->
-		<article id="contact">
-			<?php if(array_key_exists('errors', $_SESSION)): ?>
-				<div>
-					<?= implode('<br>',  $_SESSION['errors']); ?>
-				</div>
-			<?php endif; ?>
-			<?php if(array_key_exists('success', $_SESSION)): ?>
-				<div>
-					<p>Votre email a bien été envoyé</p>
-				</div>
-			<?php endif; ?>
-			<form action="post_contact.php" method="POST">
-				<input required type="text" name="name" id="inputname" value="<?= isset($_SESSION['inputs']['name']) ? $_SESSION['inputs']['name'] : '' ;?>" placeholder="Votre nom">
-				<input required type="email" name="email" id="inputemail" value="<?= isset($_SESSION['inputs']['email']) ? $_SESSION['inputs']['email'] : '' ;?>" placeholder="Votre email : exemple@exemple.com">
-				<textarea required name="message" id="inputmessage" placehoder="Votre message"><?= isset($_SESSION['inputs']['message']) ? $_SESSION['inputs']['message'] : '' ;?></textarea>
-				<button type="submit">Envoyer</button>
-			</form>
-		</article>
-	</section>
+    <section id="content">
+        <header>
+        	<a href="#presentation"><img src="img/logo.png"></a>
+            <nav>
+	            <ul>
+	            	<li><a href="#presentation">Présentation</a></li>
+	            	<li><a href="#competences">Compétences</a></li>
+	            	<li><a href="#projets">Projets</a></li>
+	            	<li><a href="#contact">Contact</a></li>
+	            </ul>
+            </nav>
+            <aside>
+                <a href="cv_kevin_lay.pdf" class="cv" title="Imprimer mon cv" download="">CV</a>
+                <a href="https://twitter.com/Lay_Kev" class="twitter" title="Allez sur mon profil twitter">Twitter</a>
+                <a href="http://www.linkedin.com/pub/kevin-lay/40/900/836" class="link" title="Visitez mon compte linkedin">Linkedin</a>
+            </aside>
+        </header>
+        <article id="presentation" class="horizontalScroll">
+            <h1>Webdesigner / Developpeur Front-End.</h1>
+            <div>
+                <h2>Kevin Lay</h2>
+                <p>Une première année en cursus Chef de Projet de Multimédia à l'IESA Multimédia, valorisé par un stage de 6 mois chez Easyvoyage.com au poste d'intégrateur. Et maintenant en formation continue en tant que développeur Multimédia à l'école Multimédia.</p>
+            </div>
+        </article>
+        <article id="competences" class="horizontalScroll">
+            <h1>Compétences Webdesign / Langages du web</h1>
+            <div class="skill">
+	                <div class="html">
+	                    <h2>HTML</h2>
+	                </div><!--
+				--><div class="css">
+	                    <h2>CSS</h2>
+	                </div><!--
+				--><div class="js">
+	                    <h2>Javascript</h2>
+	                </div><!--
+				--><div class="wdp">
+	                    <h2>Wordpress</h2>
+	                </div><!--
+				--><div class="ps">
+	                    <h2>Photoshop</h2>
+	                </div>
+            </div>
+        </article>
+        <article id="projets" class="horizontalScroll">
+        	<h1>Projets</h1>
+            <figure>
+                <a href="http://www.iesanetwork.com/w.echadli/"><img src="img/projets/portfolio_we.jpg" alt="" class="" data-source="" /></a>
+                <figcaption>Portfolio Widad Echadli</figcaption><br />
+            </figure><!--
+		--><figure>
+                <a href="http://www.iesanetwork.com/k.lay/"><img src="img/projets/folio.jpg" alt="" class="" data-source="" /></a>
+                <figcaption>Projet IESA Multimédia</figcaption><br />
+            </figure>
+        </article>
+        <article id="contact" class="horizontalScroll">
+        	<h1>Contact</h1>
+        	<div>
+        		<img src="img/K_Lay.jpg">
+	            <p>J'habite à Paris (Olympiades).<br>06.88.94.66.05<br>Vous voulez me contactez ?<br><br>Cliquez ici ► <a href="mailto:kevinlay.fr@gmail.com?subject=Contact portfolio">Contact</a></p>
+            </div>
+	        <p class="cop">© 2015 Kevin Lay</p>
+        </article>
+    </section>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="js/portfolio.js"></script>
+    <script>
+        $(document).ready(function() {
+
+            $('html, body, *').mousewheel(function(e, delta) {
+                this.scrollLeft -= (delta * 40);
+                e.preventDefault();
+            });
+
+        });
+    </script>
 </body>
 </html>
-<?php
-unset($_SESSION['inputs']);
-unset($_SESSION['success']);
-unset($_SESSION['errors']);
- ?>
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,153 @@
+$(document).ready(function(){
+
+    // Scroll Left
+
+    $('a[href^=#]').click(function(){
+        cible = $(this).attr('href');
+        if($(cible).length >= 1){
+            left = $(cible).offset().left;
+        }
+        $('html, body').animate({scrollLeft:left}, 1500);
+    });
+
+    // Width variable
+
+    var nb_element = 0;
+    var content    = document.getElementById('content');
+    var articles   = content.getElementsByTagName('article');
+
+    for(i=0; i < articles.length; i++) {
+        if(articles[i].className == 'horizontalScroll') nb_element++;
+    }
+
+    content.style.width = (nb_element * 100) + '%';
+
+    for(i=0; i < articles.length; i++) {
+        if(articles[i].className == 'horizontalScroll') articles[i].style.width = (100 / nb_element) + '%';
+    }
+});
+
+/*! Copyright (c) 2013 Brandon Aaron (http://brandonaaron.net)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
+ * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
+ * Thanks to: Seamus Leahy for adding deltaX and deltaY
+ *
+ * Version: 3.1.3
+ *
+ * Requires: 1.2.2+
+ */
+
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS style for Browserify
+        module.exports = factory;
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+
+    var toFix = ['wheel', 'mousewheel', 'DOMMouseScroll', 'MozMousePixelScroll'];
+    var toBind = 'onwheel' in document || document.documentMode >= 9 ? ['wheel'] : ['mousewheel', 'DomMouseScroll', 'MozMousePixelScroll'];
+    var lowestDelta, lowestDeltaXY;
+
+    if ( $.event.fixHooks ) {
+        for ( var i = toFix.length; i; ) {
+            $.event.fixHooks[ toFix[--i] ] = $.event.mouseHooks;
+        }
+    }
+
+    $.event.special.mousewheel = {
+        setup: function() {
+            if ( this.addEventListener ) {
+                for ( var i = toBind.length; i; ) {
+                    this.addEventListener( toBind[--i], handler, false );
+                }
+            } else {
+                this.onmousewheel = handler;
+            }
+        },
+
+        teardown: function() {
+            if ( this.removeEventListener ) {
+                for ( var i = toBind.length; i; ) {
+                    this.removeEventListener( toBind[--i], handler, false );
+                }
+            } else {
+                this.onmousewheel = null;
+            }
+        }
+    };
+
+    $.fn.extend({
+        mousewheel: function(fn) {
+            return fn ? this.bind("mousewheel", fn) : this.trigger("mousewheel");
+        },
+
+        unmousewheel: function(fn) {
+            return this.unbind("mousewheel", fn);
+        }
+    });
+
+
+    function handler(event) {
+        var orgEvent = event || window.event,
+            args = [].slice.call(arguments, 1),
+            delta = 0,
+            deltaX = 0,
+            deltaY = 0,
+            absDelta = 0,
+            absDeltaXY = 0,
+            fn;
+        event = $.event.fix(orgEvent);
+        event.type = "mousewheel";
+
+        // Old school scrollwheel delta
+        if ( orgEvent.wheelDelta ) { delta = orgEvent.wheelDelta; }
+        if ( orgEvent.detail )     { delta = orgEvent.detail * -1; }
+
+        // New school wheel delta (wheel event)
+        if ( orgEvent.deltaY ) {
+            deltaY = orgEvent.deltaY * -1;
+            delta  = deltaY;
+        }
+        if ( orgEvent.deltaX ) {
+            deltaX = orgEvent.deltaX;
+            delta  = deltaX * -1;
+        }
+
+        // Webkit
+        if ( orgEvent.wheelDeltaY !== undefined ) { deltaY = orgEvent.wheelDeltaY; }
+        if ( orgEvent.wheelDeltaX !== undefined ) { deltaX = orgEvent.wheelDeltaX * -1; }
+
+        // Look for lowest delta to normalize the delta values
+        absDelta = Math.abs(delta);
+        if ( !lowestDelta || absDelta < lowestDelta ) { lowestDelta = absDelta; }
+        absDeltaXY = Math.max(Math.abs(deltaY), Math.abs(deltaX));
+        if ( !lowestDeltaXY || absDeltaXY < lowestDeltaXY ) { lowestDeltaXY = absDeltaXY; }
+
+        // Get a whole value for the deltas
+        fn = delta > 0 ? 'floor' : 'ceil';
+        delta  = Math[fn](delta / lowestDelta);
+        deltaX = Math[fn](deltaX / lowestDeltaXY);
+        deltaY = Math[fn](deltaY / lowestDeltaXY);
+
+        // Add event and delta to the front of the arguments
+        args.unshift(event, delta, deltaX, deltaY);
+
+        return ($.event.dispatch || $.event.handle).apply(this, args);
+    }
+
+}));
+
 /*Voir le code source de matthieu*/
 
-$('.htmlcss').bind('inview', function(event, isInView) {
+/*$('.htmlcss').bind('inview', function(event, isInView) {
 	if (isInView) {
 		$(".htmlcss").animate({width:"75%"}, 1200);
 	} else {
@@ -62,4 +209,4 @@ $('.macintosh').bind('inview', function(event, isInView) {
 	} else {
 
 	}
-});
+});*/
