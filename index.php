74c333dafff786906a6c1f1a4eb72dcb46f51f98
kevin <kevinlay.fr@gmail.com>
2015-02-15 16:01:47 +0100

    correction w3c

--- index.php
+++ index.php
@@ -34,34 +34,35 @@
             <h1>Webdesigner / Developpeur Front-End</h1>
             <div>
                 <h2>Je suis <em>webdesigner</em>, <em>Developpeur Front-End</em> spécialisé dans l'intégration (langages <strong>HTML</strong> et <strong>CSS</strong>)</h2>
-                <p>Je m'appelle Kevin, je vis à Paris et depuis peu je me spécialise dans les <strong>métiers du web</strong>.J'ai réalisé ma reconversion en 2011 en intégrant le cursus <strong>Chef de Projet de Multimédia</strong> à l'<a href="http://www.iesamultimedia.fr/formation/cycle-bachelor-chef-projet-multimedia" title="Lien vers la formation de chef de projet multimédia à l'IESA Multimédia">IESA Multimédia</a>, valorisé par un stage de 6 mois chez <a href="http://www.easyvoyage.com/" title="Lien vers le site comparatif de voyage, easyvoyage.com">Easyvoyage</a> au poste d'<em>intégrateur</em>. 3 ans plus tard... le temps de financer une nouvelle formation. Je suis en formation continue <em>développeur Multimédia</em> à l'<a href="https://www.ecole-multimedia.com/formation-continue/internet-web-multimedia/informatique-multimedia-2/developpeur-web" title"Lien vers la formation continue de developpeut Multimédia à l'école Multimédia">école Multimédia</a>. Nous y avons étudier plein de langages intéressant : <strong>Javascript</strong>, <strong>jQuery</strong>, <strong>Php</strong>, <strong>Symfony</strong> et <strong>Wordpress</strong> et dans quelques jours il faudra les mettre en pratique lors d'un stage.</p>
+                <p>Je m'appelle Kevin, je vis à Paris et depuis peu je me spécialise dans les <strong>métiers du web</strong>.J'ai réalisé ma reconversion en 2011 en intégrant le cursus <strong>Chef de Projet de Multimédia</strong> à l'<a href="http://www.iesamultimedia.fr/formation/cycle-bachelor-chef-projet-multimedia" title="Lien vers la formation de chef de projet multimédia à l'IESA Multimédia">IESA Multimédia</a>, valorisé par un stage de 6 mois chez <a href="http://www.easyvoyage.com/" title="Lien vers le site comparatif de voyage, easyvoyage.com">Easyvoyage</a> au poste d'<em>intégrateur</em>. 3 ans plus tard... le temps de financer une nouvelle formation. Je suis en formation continue <em>développeur Multimédia</em> à l'<a href="https://www.ecole-multimedia.com/formation-continue/internet-web-multimedia/informatique-multimedia-2/developpeur-web" title="Lien vers la formation continue de developpeut Multimédia à l'école Multimédia">école Multimédia</a>. Nous y avons étudier plein de langages intéressant : <strong>Javascript</strong>, <strong>jQuery</strong>, <strong>Php</strong>, <strong>Symfony</strong> et <strong>Wordpress</strong> et dans quelques jours il faudra les mettre en pratique lors d'un stage.</p>
             </div>
         </article>
         <article id="competences" class="horizontalScroll">
             <h1>Compétences Webdesign / Langages du web</h1>
             <div class="skill">
-	                <div class="html">
-	                    <h2>HTML</h2>
-	                </div><!--
-				--><div class="css">
-	                    <h2>CSS</h2>
-	                </div><!--
-				--><div class="jq">
-	                    <h2>jQuery</h2>
-	                </div><!--
-				--><div class="wdp">
-	                    <h2>Wordpress</h2>
-	                </div><!--
-				--><div class="ps">
-	                    <h2>Photoshop</h2>
-	                </div>
+                <div class="html">
+                    <h2>HTML</h2>
+                </div><!--
+			--><div class="css">
+                    <h2>CSS</h2>
+                </div><!--
+			--><div class="jq">
+                    <h2>jQuery</h2>
+                </div><!--
+			--><div class="wdp">
+                    <h2>Wordpress</h2>
+                </div><!--
+			--><div class="ps">
+                    <h2>Photoshop</h2>
+                </div>
             </div>
+            <p>Le <strong>Html</strong> représente la plus grande partie de mes compétences avec le <strong>Css</strong>, c'est les premiers langages que j'ai appris Et sans <strong>Html</strong>, pas de contenu donc pas de site ! Puis de nos jours, on aime avoir de joli site donc le <strong>Css</strong> est primordial. Pour être des plus complets, tout du moins pour devenir un developpeut Front-End, il faut apprendre d'autre langages. C'est la que je débute donc l'apprentissage de <strong>javascript</strong>, et surtout de <strong>jQuery</strong> ainsi que d'au moins un <em>CMS</em>, <strong>Wordpress</strong>. Je suis aussi capable de concevoir des maquettes, notamment grâce à <strong>Photoshop</strong> !</p>
         </article>
         <article id="projets" class="horizontalScroll">
         	<h1>Projets</h1>
             <figure>
             	<img src="img/projets/crf.jpg" alt="Image de présentation du site locale de la croix-rouge Villiers/Bry/plessis" />
-                <figcaption>► Site locale de la croix-rouge en cours de déploiement.</a></figcaption>
+                <figcaption>► Site locale de la croix-rouge en cours de déploiement.</figcaption>
             </figure><!--
 		--><figure>
 			<img src="img/projets/portfolio_we.jpg" alt="Image de présentation du projet de portfolio de Widad Echadli" />
